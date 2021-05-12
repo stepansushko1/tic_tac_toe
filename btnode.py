df6af5316235b95027703a83cda0e7cee2da2991
Stepan Seppukku <stepan.sushko@ucu.edu.ua>
2021-05-12 20:19:57 +0300

    Commit with game

---
+++ btnode.py
@@ -0,0 +1,10 @@
+"""
+File: bstnode.py
+Author: Ken Lambert
+"""
+
+class BSTNode:
+    """Represents a node for a linked binary search tree."""
+
+    def __init__(self, data):
+        self.data = data
